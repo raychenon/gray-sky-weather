1ff198777b7fdd0d08134d6f04f703c94b2f4270
Raymond Chenon <raymond.chenon@gmail.com>
2020-08-26 21:30:52 +0200

    fix tests

--- app/src/test/java/io/betterapps/graysky/data/models/WeatherUnitTest.kt
+++ app/src/test/java/io/betterapps/graysky/data/models/WeatherUnitTest.kt
@@ -9,22 +9,22 @@ class WeatherUnitTest {
 
     @Test
     fun actualLocalTimeTest() {
-        assertEquals("17:00", weatherUnit.actualLocalTime())
+        assertEquals("15:00", weatherUnit.actualLocalTime())
     }
 
     @Test
     fun offsetLocalTimeTest() {
         // timezone: "Europe/Berlin", time offset: 7200
         // timeOffset is in seconds
-        assertEquals("19:00", weatherUnit.actualLocalTime(timeOffset = 7200))
+        assertEquals("17:00", weatherUnit.actualLocalTime(timeOffset = 7200))
     }
 
     @Test
     fun localTimeformattingTest() {
-        assertEquals("17:00:00", weatherUnit.actualLocalTime(format = "HH:mm:ss"))
-        assertEquals("05:00:00", weatherUnit.actualLocalTime(format = "hh:mm:ss"))
+        assertEquals("15:00:00", weatherUnit.actualLocalTime(format = "HH:mm:ss"))
+        assertEquals("03:00:00", weatherUnit.actualLocalTime(format = "hh:mm:ss"))
         assertEquals(
-            "2020-08-26T19:00:00",
+            "2020-08-26T17:00:00",
             weatherUnit.actualLocalTime(timeOffset = 7200, format = "yyyy-MM-dd'T'HH:mm:ss")
         )
     }
@@ -33,11 +33,11 @@ class WeatherUnitTest {
     fun mockNewYorkTest() {
         val newYorkWeather = WeatherUnit(1598464458, 25f, 21.61f, 72, 75, emptyList())
         assertEquals(
-            "2020-08-26T15:54:18",
+            "2020-08-26T13:54:18",
             newYorkWeather.actualLocalTime(timeOffset = -14400, format = "yyyy-MM-dd'T'HH:mm:ss")
         )
         assertEquals(
-            "15:54",
+            "13:54",
             newYorkWeather.actualLocalTime(timeOffset = -14400)
         )
     }
@@ -47,14 +47,14 @@ class WeatherUnitTest {
         val newYorkWeather = WeatherUnit(1598464863, 22.59f, 23.66f, 72, 75, emptyList())
         val timeOffset = -25200L
         assertEquals(
-            "2020-08-26T13:01:03",
+            "2020-08-26T11:01:03",
             newYorkWeather.actualLocalTime(
                 timeOffset = timeOffset,
                 format = "yyyy-MM-dd'T'HH:mm:ss"
             )
         )
         assertEquals(
-            "13:01",
+            "11:01",
             newYorkWeather.actualLocalTime(timeOffset = timeOffset)
         )
     }
