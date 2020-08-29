bdb5502e13b8f6c48d7339ab8940612b8c0af4e7
Jean-Michel Fayard <jmfayard@gmail.com>
2020-08-29 21:20:35 +0200

    ♻️ gradle: convert Groovy to Kotlin at the top-level
    With help from
    https://github.com/bernaferrari/GradleKotlinConverter

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val kotlin_version = "1.4.0"
+    repositories {
+        google()
+        jcenter()
+        maven(
+                "https://plugins.gradle.org/m2/"
+        )
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath(kotlin("gradle-plugin", version = "$kotlin_version"))
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.3.0")
+        classpath("com.google.gms:google-services:4.3.3")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.0"
+}
+
+
+subprojects {
+    /**
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    // Optionally configure plugin
+
+    ktlint {
+        debug = true
+    }
+    **/
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
