9b9c5b8395cb79958fb57ef2eec39c3b0252e837
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-16 23:20:55 +0200

    #8 autocomplete  activity

--- app/src/main/java/io/betterapps/graysky/MainActivity.kt
+++ app/src/main/java/io/betterapps/graysky/MainActivity.kt
@@ -1,11 +1,19 @@
 package io.betterapps.graysky
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.location.Location
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import es.dmoral.toasty.Toasty
 import io.betterapps.graysky.const.GlobalConstants
 import io.betterapps.graysky.data.domains.GeoLocation
@@ -13,6 +21,7 @@ import io.betterapps.graysky.data.domains.LocationName
 import io.betterapps.graysky.geoloc.UserLocationDelegate
 import io.betterapps.graysky.ui.main.MainViewModel
 import io.betterapps.graysky.ui.weatherforecast.WeatherForecastFragment
+import kotlinx.android.synthetic.main.main_activity.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -49,6 +58,17 @@ class MainActivity : AppCompatActivity() {
                 displayWeatherFromLocations(GlobalConstants.USER_LOCATION)
             }
         }
+
+        // Initialize the SDK
+        Places.initialize(applicationContext, getString(R.string.cloud_platform_api))
+
+        // Create a new PlacesClient instance
+        val placesClient: PlacesClient = Places.createClient(this)
+
+
+        main_search_button.setOnClickListener(View.OnClickListener {
+            launchAutocomplete()
+        })
     }
 
     fun showWeatherFragments(locations: List<LocationName>) {
@@ -80,12 +100,52 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
+
+    private fun launchAutocomplete(): Unit {
+
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        val fields = listOf(Place.Field.ID, Place.Field.NAME)
+
+        // Start the autocomplete intent.
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+            .build(this)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Timber.i("onActivityResult Place: ${place.name}, ${place.id}")
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Timber.i("onActivityResult ${status.statusMessage}")
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    Timber.i("onActivityResult cancelled")
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
     private fun showRationale() {
         Toasty.error(this, R.string.permission_rationale, Toast.LENGTH_SHORT, true).show()
     }
 
     private fun showErrorLastLocation() {
-        Toasty.error(this, R.string.permission_last_location_not_found, Toast.LENGTH_LONG, true).show()
+        Toasty.error(this, R.string.permission_last_location_not_found, Toast.LENGTH_LONG, true)
+            .show()
     }
 
     private fun displayWeatherOrError(location: Location?) {
