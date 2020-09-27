d7ef36df20cf06e55d22aba092ad11896b9eb1ee
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-27 22:32:15 +0200

    #8 remove the  R.id.weather_hour_rain_textview

--- app/src/main/java/io/betterapps/graysky/ui/adapter/HourlyWeatherViewHolder.kt
+++ app/src/main/java/io/betterapps/graysky/ui/adapter/HourlyWeatherViewHolder.kt
@@ -25,8 +25,6 @@ class HourlyWeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
     val timeTextView: TextView = itemView.findViewById(R.id.weather_hour_time_textview)
     val realTempTextView: TextView = itemView.findViewById(R.id.weather_hour_real_temp_textview)
     val dateTextView: TextView = itemView.findViewById(R.id.weather_date_textview)
-    val rainTextView: TextView =
-        itemView.findViewById(R.id.weather_hour_rain_textview)
     val iconView: ImageView = itemView.findViewById(R.id.weather_icon_imageview)
 
     override val containerView: View?
@@ -35,13 +33,7 @@ class HourlyWeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
     fun bindData(item: WeatherUnit, timezoneOffset: Long) {
         timeTextView.text = item.actualLocalTime(timeOffset = timezoneOffset)
         realTempTextView.text = formatTemperature(item.temperature)
-        // item.rain?.let {
-        //     rainTextView.visibility = View.VISIBLE
-        //     // todo find a better a wording
-        //     rainTextView.text = getString(R.string.rain_volume_format, it.precipitationVolumeForecast1h)
-        // } ?: run {
-        //     rainTextView.visibility = View.GONE
-        // }
+
         ImageLoader.load(iconView, item.iconURL())
 
         if (item.isBeforeNewDay(timezoneOffset)) {
