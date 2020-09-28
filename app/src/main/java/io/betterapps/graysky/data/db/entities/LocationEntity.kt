2adf4281d8520606035c87c98913b5ecb7f257e0
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-28 23:38:40 +0200

    #14 create the entity

---
+++ app/src/main/java/io/betterapps/graysky/data/db/entities/LocationEntity.kt
@@ -0,0 +1,13 @@
+package io.betterapps.graysky.data.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "location_table")
+data class LocationEntity(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val referenceID: String, //ChIJOwg_06VPwokRYv534QaPC8g
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "latitude") val latitude: Double,
+    @ColumnInfo(name = "longitude") val longitude: Double
+)
