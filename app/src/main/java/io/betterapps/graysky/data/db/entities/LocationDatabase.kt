b6af581c2d3007a8e016753d115d16735084eebe
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-29 00:25:43 +0200

    #14 add database

---
+++ app/src/main/java/io/betterapps/graysky/data/db/entities/LocationDatabase.kt
@@ -0,0 +1,35 @@
+package io.betterapps.graysky.data.db.entities
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(LocationEntity::class), version = 1, exportSchema = false)
+public abstract class LocationDatabase : RoomDatabase() {
+
+    abstract fun locationDao(): LocationDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: LocationDatabase? = null
+
+        fun getDatabase(context: Context): LocationDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LocationDatabase::class.java,
+                    "location_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
