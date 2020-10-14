3a3dfe51d28493a07b7866697c36f9a26e80d898
Raymond Chenon <raymond.chenon@gmail.com>
2020-10-14 21:32:01 +0200

    update refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 import de.fayard.refreshVersions.bootstrapRefreshVersions
+import de.fayard.refreshVersions.migrateRefreshVersionsIfNeeded
 
 buildscript {
     val useSnapshot = false
     repositories { mavenLocal() ; gradlePluginPortal() }
     dependencies.classpath(
         if (useSnapshot) "de.fayard.refreshVersions:refreshVersions:0.9.6-SNAPSHOT" else "de.fayard.refreshVersions:refreshVersions:0.9.5"
+////                                                                                                                    # available:0.9.6"
 
     )
 }
@@ -22,5 +24,7 @@ gradleEnterprise {
 
 include(":app")
 rootProject.name = "GraySky"
+
+migrateRefreshVersionsIfNeeded("0.9.5") // Will be automatically removed by refreshVersions when upgraded to the latest version.
 
 bootstrapRefreshVersions()
